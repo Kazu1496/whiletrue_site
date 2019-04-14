b2913251ceaee1e85fb248c49d1220d9b059fbe2
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-04-15 00:54:53 +0900

    Add: OGP周り追加/スタイル諸々修正

--- source/javascripts/merge.js
+++ source/javascripts/merge.js
@@ -1,1 +1,20 @@
-// This is where it all goes :)
+$(function(){
+  $(window).on('load', function(){
+    $('#loading_container > p').fadeOut(2000, function(){
+      $('#loading_container > em').fadeIn(2000, function(){
+        setTimeout(function(){
+          $('#loading_container').fadeOut(1000);
+        }, 1000);
+      });
+    });
+  });
+
+  $(window).scroll(function(){
+    var window_height = $(window).height();
+    if($(window).scrollTop() > window_height - 100){
+      $('header').addClass("moved");
+    } else {
+      $('header').removeClass("moved");
+    };
+  });
+});
